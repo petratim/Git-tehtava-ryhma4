28a8366dc495a6edbb9bc068fe6592a745ad5440
Karoliina <142309693+KaroliinaE@users.noreply.github.com>
2023-12-04 10:41:39 +0200

    Metodit

--- src/gitTehtäväRyhmä4.java
+++ src/gitTehtäväRyhmä4.java
@@ -4,12 +4,29 @@ public class gitTehtäväRyhmä4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+	}//Enf of main
 	
 class Auto {
+
+	//Attribuutit
 	public String merkki;
 	public String Malli;
 	public int bensanMaara;
 }
+	//Metodit
+	public void Jarruta () {
+
+		System.out.println("Auto jarruttaa.")
+	}
+
+	public void Kiihdyta () {
+
+		System.out.println("Auto kiihtyy.")
+	}
+
+	public void NaytaTiedot () {
+
+		bensanMaara = bensanMaara - 1;
+	}
 
 }
